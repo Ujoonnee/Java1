3aaf075fdd4aa48ea2f333cfcb89f16596d8730e
Ujoonnee <69345466+Ujoonnee@users.noreply.github.com>
2021-11-06 15:32:25 +0900

    Create OkJavaGoinHomeInput.java

---
+++ OkJavaGoinHomeInput.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Security;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.DimmingLights;
+
+public class OkJavaGoinHomeInput {
+
+	public static void main(String[] args) {
+
+		String id = JOptionPane.showInputDialog("Enter your id");
+		String bright = JOptionPane.showInputDialog("brightness");
+		
+		// call Elevator
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForUp(1);
+		
+		// Security off
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+		
+		// light on
+		Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+		hallLamp.on();
+		
+		Lighting floorLamp = new Lighting(id+" / floor Lamp");
+		floorLamp.on();
+		
+		DimmingLights moodLamp = new DimmingLights(id+" moodLamp");
+		moodLamp.setBright(Double.parseDouble(bright));
+		moodLamp.on();
+
+	}
+
+}
