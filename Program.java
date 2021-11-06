abd2454e97779216ec886c3d00613c45e798ce7a
Ujoonnee <69345466+Ujoonnee@users.noreply.github.com>
2021-11-06 15:07:34 +0900

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,15 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		
+		// line 6 실행 후 line 7, 이후 line 8이 실행된다.
+		// 프로그래밍을 통해 자동화 시킨 것
+
+	}
+
+}
