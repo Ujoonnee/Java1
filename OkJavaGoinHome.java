e3311bef9271d0b18a73d083e83be63c073cce0d
Ujoonnee <69345466+Ujoonnee@users.noreply.github.com>
2021-11-06 15:07:01 +0900

    Create OkJavaGoinHome.java

---
+++ OkJavaGoinHome.java
@@ -0,0 +1,28 @@
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Security;
+import org.opentutorials.iot.Lighting;
+
+public class OkJavaGoinHome {
+
+	public static void main(String[] args) {
+
+		String id = "JAVA APT 1007";
+		
+		// call Elevator
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForUp(1);
+		
+		// Security off
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+		
+		// light on
+		Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+		hallLamp.on();
+		
+		Lighting floorLamp = new Lighting(id+" / floor Lamp");
+		floorLamp.on();
+
+	}
+
+}
