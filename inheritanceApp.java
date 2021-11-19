1170e0b9637f45360740e6d1626cc6c7f8a71505
Ujoonnee <69345466+Ujoonnee@users.noreply.github.com>
2021-11-19 16:21:37 +0900

    Update inheritanceApp.java
    this, super

--- inheritanceApp.java
+++ inheritanceApp.java
@@ -1,29 +1,27 @@
 class Cal { 
-	public int sum(int v1, int v2) {
-		return v1+v2;
+	int v1, v2;
+	Cal(int v1, int v2){
+		System.out.println("Cal init!!");
+		this.v1 = v1; this.v2 = v2;
 	}
+	public int sum() {return this.v1+v2;}
 }
 class Cal3 extends Cal {
-	public int minus(int v1, int v2) {
-		return v1-v2;
+
+	Cal3(int v1, int v2) {
+		super(v1, v2);	// execute the constructor of super class
+		System.out.println("Cal3 init!!");
+		
 	}
-	// Overriding
-	public int sum(int v1, int v2) {
-		System.out.println("Cal3!!");
-		return v1+v2;
-	}
-	
+	public int minus() {return this.v1-v2;}
 }
 
 
 public class InheritanceApp {
 	public static void main(String[] args) {
-		Cal c = new Cal();
-		System.out.println(c.sum(2, 1));
-		
-		Cal3 c3 = new Cal3();
-		System.out.println(c3.sum(2,2));
-		System.out.println(c3.minus(2,1));
-		System.out.println(c3.sum(2,2));
+		Cal c = new Cal(2,1);
+		Cal3 c3 = new Cal3(2,1);
+		System.out.println(c3.sum());
+		System.out.println(c3 .minus());
 	}
 }
